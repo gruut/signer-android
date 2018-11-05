618509c0be39a9259d09c589a2624cb813e1a047
Minyoung Song <song@thevaulters.com>
2018-11-05 09:28:07 +0900

    Initial commit

---
+++ app/src/main/java/com/gruutnetworks/gruutsigner/MainActivity.java
@@ -0,0 +1,19 @@
+package com.gruutnetworks.gruutsigner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.gruutnetworks.gruutsigner.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
