b95773a5d105d66734ece26dc559066a70d9116e
Minyoung Song <song@thevaulters.com>
2018-11-06 14:15:11 +0900

    grpc test
    * 하드코딩 된 포트로 ping 주고받기
    * proto를 위한 plugin 세팅

--- app/src/main/java/com/gruutnetworks/gruutsigner/ui/main/MainFragment.java
+++ app/src/main/java/com/gruutnetworks/gruutsigner/ui/main/MainFragment.java
@@ -1,6 +1,7 @@
 package com.gruutnetworks.gruutsigner.ui.main;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,10 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.gruutnetworks.gruutsigner.R;
+import com.gruutnetworks.gruutsigner.databinding.MainFragmentBinding;
 
 public class MainFragment extends Fragment {
 
-    private MainViewModel mViewModel;
+    private MainViewModel viewModel;
+    private MainFragmentBinding binding;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -22,14 +25,17 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false);
+
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        binding.setModel(viewModel);
     }
+
 
 }
--- app/src/main/java/com/gruutnetworks/gruutsigner/ui/main/MainViewModel.java
+++ app/src/main/java/com/gruutnetworks/gruutsigner/ui/main/MainViewModel.java
@@ -1,7 +1,67 @@
 package com.gruutnetworks.gruutsigner.ui.main;
 
 import android.arch.lifecycle.ViewModel;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+import com.gruutnetworks.gruutsigner.GreeterGrpc;
+import com.gruutnetworks.gruutsigner.HelloReply;
+import com.gruutnetworks.gruutsigner.HelloRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.concurrent.TimeUnit;
 
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private static final String TAG = "MainViewModel";
+
+    public void onClickButton() {
+        new GrpcTask()
+                .execute(
+                        "10.10.10.106",
+                        "Hello? ",
+                        "50051");
+    }
+
+
+    private static class GrpcTask extends AsyncTask<String, Void, String> {
+        private ManagedChannel channel;
+
+        private GrpcTask() {
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String host = params[0];
+            String message = params[1];
+            String portStr = params[2];
+            int port = TextUtils.isEmpty(portStr) ? 0 : Integer.valueOf(portStr);
+            try {
+                channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+                GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
+                HelloRequest request = HelloRequest.newBuilder().setName(message).build();
+                HelloReply reply = stub.sayHello(request);
+                return reply.getMessage();
+            } catch (Exception e) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                pw.flush();
+                return String.format("Failed... : %n%s", sw);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            Log.d(TAG, "Result: " + result);
+        }
+    }
 }
