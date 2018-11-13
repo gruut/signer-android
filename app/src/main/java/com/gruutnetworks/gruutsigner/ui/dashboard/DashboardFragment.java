3e27d1d13361f1d203def970562a0a368f7c9b9f
Minyoung <song@thevaulters.com>
2018-11-13 14:49:13 +0900

    # Dashboard Activity Skeleton 생성

---
+++ app/src/main/java/com/gruutnetworks/gruutsigner/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,35 @@
+package com.gruutnetworks.gruutsigner.ui.dashboard;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.gruutnetworks.gruutsigner.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel mViewModel;
+
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/gruutnetworks/gruutsigner/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,7 @@
+package com.gruutnetworks.gruutsigner.ui.dashboard;
+
+import android.arch.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
