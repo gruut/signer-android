f75f813366187ed8ba746a0eb70a93e22b4c2faf
Junsuk Park <park64kr63@gmail.com>
2018-12-05 23:24:24 +0900

    # EDIT - signData
    ## 수정사항
    - 종전의 코드에서는 `signData`에 string을 넘겨주고 있었는데, string을 다시 bytes로 변환해서 서명을 하고 있기 때문에 string이 아닌 bytes로 넘겨주는게 낫다.
    - 특히 OutputStream의 경우 1. bytes[] -> 2. string(toString) -> 3. bytes[](getBytes) 형변환을 하게 되면 1번과 3번의 크기의 차이가 발생한다.
    - signData를 bytes로 받도록 수정

--- app/src/main/java/com/gruutnetworks/gruutsigner/util/KeystoreUtil.java
+++ app/src/main/java/com/gruutnetworks/gruutsigner/util/KeystoreUtil.java
@@ -210,11 +210,9 @@ public class KeystoreUtil {
      *
      * @return A string encoding of the data signature generated
      */
-    public String signData(String inputStr)
+    public String signData(byte[] data)
             throws KeyStoreException, UnrecoverableEntryException, NoSuchAlgorithmException,
             InvalidKeyException, SignatureException, IOException, CertificateException {
-        byte[] data = inputStr.getBytes();
-
         KeyStore ks = KeyStore.getInstance(KEYSTORE_PROVIDER_ANDROID_KEYSTORE);
         ks.load(null);
 
