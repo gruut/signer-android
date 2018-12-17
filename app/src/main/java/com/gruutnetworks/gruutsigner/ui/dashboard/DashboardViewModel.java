34e77d248441ae8f09ead2ff6b0c8634018ae384
Minyoung <song@thevaulters.com>
2018-12-17 15:32:16 +0900

    GEN-49 서명한 블록의 chain id와 블록 높이를 저장
    * 내부 DB에 서명한 블록의 chain id와 블록 height를 저장할 수 있도록 함.
    * chain_id와 block_hgt를 결합하여 primary key로 사용.
    * 이미 있는 블록 정보라면, fail이 떠서 exception 처리하도록

--- app/src/main/java/com/gruutnetworks/gruutsigner/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/gruutnetworks/gruutsigner/ui/dashboard/DashboardViewModel.java
@@ -42,6 +42,7 @@ public class DashboardViewModel extends AndroidViewModel implements LifecycleObs
     private AuthCertUtil authCertUtil;
     private AuthHmacUtil authHmacUtil;
     private PreferenceUtil preferenceUtil;
+    private SignedBlockDao blockDao;
 
     private ManagedChannel channel1;
     private ManagedChannel channel2;
@@ -58,6 +59,8 @@ public class DashboardViewModel extends AndroidViewModel implements LifecycleObs
         this.authHmacUtil = AuthHmacUtil.getInstance();
         this.preferenceUtil = PreferenceUtil.getInstance(application.getApplicationContext());
         this.sender = preferenceUtil.getString(PreferenceUtil.Key.SID_STR);
+
+        blockDao = AppDatabase.getDatabase(application).blockDao();
 
         if (!NetworkUtil.isConnected(application.getApplicationContext())) {
             SnackbarMessage snackbarMessage = new SnackbarMessage();
@@ -356,6 +359,11 @@ public class DashboardViewModel extends AndroidViewModel implements LifecycleObs
         String time = AuthGeneralUtil.getTimestamp();
         String signature;
         try {
+            SignedBlock block = new SignedBlock();
+            block.setChainId(msgRequestSignature.getChainId());
+            block.setBlockHeight(msgRequestSignature.getBlockHeight());
+            blockDao.insertAll();
+
             signature = authCertUtil.generateSupportSignature(sender, time, msgRequestSignature.getmID(), GruutConfigs.localChainId,
                     msgRequestSignature.getBlockHeight(), msgRequestSignature.getTransaction());
             logMerger1.postValue("Signature generated!");
